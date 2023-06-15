bc971525b56c5abae19e3cce02eaf4c831cc1f2b
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-06-15 16:44:58 +0200

    Add a form and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-    
+      <form>
+        <label>First Name:</label>
+        <input type="text" />
+        <label>Last Name:</label>
+        <input type="text" />
+        <label>Email:</label>
+        <input type="email" />
+        <label>Age:</label>
+        <input type="number" />
+        <label>Password:</label>
+        <input type="password" />
+        <label>Confirm Password:</label>
+        <input type="password" />
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
